3180d4205cad5305bdfcf9c7100ce6e823f43991
Carol Xu <xucarol.20@gmail.com>
2026-09-10 12:25:59 -0400

    ʕ｡- ˕ -｡ʔ ᶻ 𝗓

--- data-code/acs_race_age_recode.R
+++ data-code/acs_race_age_recode.R
@@ -1,6 +1,6 @@
 ## Preliminaries -----------------------------------------------------------
 if (!require("pacman")) install.packages("pacman")
-pacman::p_load(tidyverse, ggthemes, readxl, data.table, gdata, ipumsr)
+pacman::p_load(tidyverse, ggthemes, readxl, data.table, gdata, ipumsr, icpsrdata)
 
 # Set working directory 
 setwd("C:/Users/CarolXu/OneDrive - Cato Institute/Desktop/NIBRS Homicides 2021-2024")
